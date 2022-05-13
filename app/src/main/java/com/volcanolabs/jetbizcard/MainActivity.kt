d76674ce998690dbc93d2d41a1b7d5336af8b848
Luis Lugo <luis.lugo@shockoe.com>
2022-05-13 16:01:48 -0500

    final version of bizcard

--- app/src/main/java/com/volcanolabs/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/volcanolabs/jetbizcard/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,11 +15,15 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.volcanolabs.jetbizcard.ui.theme.JetbizcardTheme
@@ -40,6 +45,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
 fun CreateBizCard() {
     Surface(
@@ -122,22 +128,6 @@ fun CreateCardInfoSection() {
 }
 
 @Composable
-fun CreatePortfolioSection() {
-    Button(
-        onClick = {
-            Log.d("Clicked", "CreateBizCard: Clicked")
-        },
-        modifier = Modifier.padding(4.dp),
-    ) {
-        Text(
-            text = "Portfolio",
-            style = MaterialTheme.typography.button
-        )
-    }
-}
-
-@Preview
-@Composable
 fun Content() {
     Box(
         modifier = Modifier
@@ -164,10 +154,66 @@ fun Content() {
 }
 
 @Composable
+fun CreatePortfolioSection() {
+    var buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+    Button(
+        onClick = {
+            Log.d("Clicked", "CreateBizCard: Clicked")
+            buttonClickedState.value = !buttonClickedState.value
+        },
+        modifier = Modifier.padding(4.dp),
+    ) {
+        Text(
+            text = "Portfolio",
+            style = MaterialTheme.typography.button
+        )
+    }
+
+    if (buttonClickedState.value) {
+        Content()
+    } else {
+        Box {}
+    }
+}
+
+@Composable
 fun Portfolio(data: List<String>) {
     LazyColumn {
         items(data) { item ->
-            Text(item)
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(7.dp)
+                ) {
+                    CreateImageProfile(
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically),
+                    ) {
+                        Text(
+                            text = item,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "A great project",
+                            style = MaterialTheme.typography.body2
+                        )
+                    }
+                }
+            }
         }
     }
 }
